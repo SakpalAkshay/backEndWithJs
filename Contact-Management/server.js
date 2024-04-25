153a2097f2a14cd17d7ea8c7bcd12ba1d23696d5
Akshay Sakpal <akki.sakp01@protonmail.com>
2024-04-25 14:38:25 -0700

    Added Connection Settings

--- Contact-Management/server.js
+++ Contact-Management/server.js
@@ -1,6 +1,7 @@
 // Import the Express framework module
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
+const connectDb = require("./config/dbConnection");
 
 
 //loads the variables from the .env file into the process.env object. The .env file typically contains key-value pairs of environment variables
@@ -8,6 +9,10 @@ const dotenv = require("dotenv").config();
 
 // Create an Express application instance
 const app = express();
+
+//connecting to DB
+connectDb();
+
 
 
 //sets the port variable to either the value of the PORT environment variable (if it is set) or defaults to 5000 if the PORT environment variable is not defined.
