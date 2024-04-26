40d8da451959dac34d35969bad82b59bf4b7c186
Akshay Sakpal <akki.sakp01@protonmail.com>
2024-04-26 15:06:04 -0700

    Updated Constants Data

--- Contact-Management/constants.js
+++ Contact-Management/constants.js
@@ -1,10 +1,10 @@
 
 // Export the constants using the `exports` object
 exports.constants ={
-    "VALIDATION_ERROR": 400,
-    "UNAUTHORIZED": 401,
+    "VALIDATION_ERROR":400,
+    "UNAUTHORIZED":401,
     "FORBIDDEN":403,
-    "NOT_FOUND": 404,
+    "NOT_FOUND":404,
     "SERVER_ERROR": 500
 
 }
