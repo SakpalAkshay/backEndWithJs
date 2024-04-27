6cde0c7fa39eedd9e77937b202efb42a14fa98a8
Akshay Sakpal <akki.sakp01@protonmail.com>
2024-04-27 15:49:14 -0700

    Added Password Hashing and User registration

--- Contact-Management/controllers/userController.js
+++ Contact-Management/controllers/userController.js
@@ -2,6 +2,10 @@
 //For this we EXPRESS ASYNC handler
 //The express-async-handler package is commonly used in Express.js applications to handle asynchronous functions and middleware errors in a more streamlined manner.
 const asyncHandler = require("express-async-handler");
+const brcypt = require("bcrypt");
+const User = require("../models/userModel");
+
+
 
 //@desc Get Current user
 //@route GET/api/user/current
@@ -14,8 +18,34 @@ const currentUser = asyncHandler(async (req,res)=>{
 //@route POST/api/user/register
 //@access public
 const registerUser = asyncHandler(async (req,res) =>{
-    res.status(201).json({message:"Registering User"});
-})
+
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email}); //need to find based upon a field cause we are passing JSON
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered");
+    }
+ 
+    //now after all checks lets Hash the password
+    //await: Since bcrypt.hash returns a promise
+    //bcrypt.hash(password, 10): This code uses the hash method provided by the bcrypt library to hash the plaintext password (password) with a specified cost factor (10). The cost factor (also known as the number of hashing rounds) determines the computational effort required to hash the password
+    const hashedPassword = await brcypt.hash(password,10);
+    //creating user
+    const user = await User.create({username,email,password:hashedPassword}); //username and email fields are same but we modified the password
+    console.log("Created User",user);
+    if (user){
+        res.status(201).json({_id : user.id, email: user.email});
+    } else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+    
+
+});
 
 
 //@desc POST Login User
