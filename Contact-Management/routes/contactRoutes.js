efc30c8cfb017c95be46e4cfd7ec3e4097a75490
Akshay Sakpal <akki.sakp01@protonmail.com>
2024-05-01 15:31:24 -0700

    Added Middleware for token Validation

--- Contact-Management/routes/contactRoutes.js
+++ Contact-Management/routes/contactRoutes.js
@@ -3,7 +3,9 @@ const express = require('express');
 //Routers are used to group route handlers for a specific part of an application.
 const router = express.Router();
 const {getContacts, createContact, updateContact, deleteContact, getContact } = require("../controllers/contactController");
-
+const validateToken = require('../middlewares/validateTokenHandler');
+//Protecting the Contact routes with Validation of User so that only user can Update them
+router.use(validateToken);
 
 //a route for the root path ("/") using the get method of the router instance.
 router.route("/").get(getContacts).post(createContact); //Mutiple HTTP Methods through a single Route
