1223de2b9325eb7fbb465f69ae3cdde1356d6dbc
Akshay Sakpal <akki.sakp01@protonmail.com>
2024-05-01 15:11:18 -0700

    Added Middleware to User/Current End Point

--- Contact-Management/routes/userRoutes.js
+++ Contact-Management/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { registerUser, loginUser, currentUser } = require('../controllers/userController');
-
+const validateToken = require('../middlewares/validateTokenHandler');
 //Routers are used to group route handlers for a specific part of an application.
 const router = express.Router();
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/current').get(currentUser);
+router.route('/current').get(validateToken,currentUser);
 
 module.exports = router;
