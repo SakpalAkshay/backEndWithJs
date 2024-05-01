9d54d32eb9d33962e2a7a48d9a619c7206bd71b5
Akshay Sakpal <akki.sakp01@protonmail.com>
2024-05-01 15:18:49 -0700

    Added Comments for code explanation

--- Contact-Management/middlewares/validateTokenHandler.js
+++ Contact-Management/middlewares/validateTokenHandler.js
@@ -3,9 +3,11 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async(req,res,next)=>{ //its a middleware so we need these many parameters
     let token;
-    let authHeader = req.headers.authorization || req.headers.Authorization;
-
+    //The authorization header name is case-sensitive according to HTTP standards.
+    let authHeader = req.headers.authorization || req.headers.Authorization; //attempts to access the "Authorization" header with a capital "A"
+    //checks if the authHeader string starts with the "Bearer" scheme. In the OAuth 2.0 authentication standard, tokens are often sent in the format "Bearer <token>"
     if(authHeader && authHeader.startsWith("Bearer")){
+        //authHeader.split(" ")[1]: This accesses the second element of the array created by split(" "), which corresponds to the token portion after "Bearer"
         token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
             if(err){
